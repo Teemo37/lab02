e64b3eed7b37f664e84e6ca0fd4a2cb1912b18ca
Teemo37 <teemo.andronov@gmail.com>
2022-03-21 14:08:03 +0300

    fix

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,6 +5,11 @@ int
 main()
 {
   std::string name;
+<<<<<<< HEAD
   std::cin >> name;  
   std::cout << "Hello World, " << name; // hello world + name
+=======
+  std::cin >> name;
+  std::cout << "Hello World " << name; // hello world
+>>>>>>> style
 }
