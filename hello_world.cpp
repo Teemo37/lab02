feaa282c86503e975f5df5bdcb8c5273a39a5a6b
Teemo37 <89978094+Teemo37@users.noreply.github.com>
2022-03-21 14:01:54 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,5 +5,5 @@ int main()
 {
   std::string name;
   std::cin >> name;  
-  std::cout << "Hello World, " << name; // hello world + name
+  std::cout << "Hello World, " << name; // Привет мир + имя
 }
